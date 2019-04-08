2e9408e5863a9b23cc79920b5485ff1e0c407fc0
asouil <amaella.souil@gmail.com>
2019-04-08 11:38:22 +0200

    corrigé

--- index.php
+++ index.php
@@ -7,68 +7,71 @@
 
 	<?php
 	echo('Exercice 1 <br/>');
-		for($var=0;$var<10; $var++){
-		print($var.'<br/>');
-
+	$var=0;
+	while($var<10){
+		print($var.'; ');
+		$var++;
 	}
 	?>
 
 
 	<?php
-	echo('Exercice 2 <br/>');
+	echo('<br/> Exercice 2 <br/>');
 	$var = 0;
 	$var2 = 13;
-	$var3=0;
-	while($var*$var2<20){
+	$var3 = 0; //pas indispensable : echo $var*$var2;
+	while($var<=20){
 		$var3=$var*$var2;
-		print($var3.'<br>');
+		print($var3.'; ');
 		$var++;
 	}
 	?>
 
 	<?php
-	echo('Exercice 3 <br/>');
-	$var=100;
-	$var2=45;
-	$var3=0;
-	while($var>10){
-		$var3=$var*$var2;
-		print($var3.'<br>');
-		$var--;
+	echo('<br/> Exercice 3 <br/>');
+	$var4=100;
+	$var5=45;
+	$result=0;
+	while($var4>10){
+		$result=$var4*$var5;
+		print($result.'; ');
+		$var4--;
 	}
 	?>
+
 	<?php
-	echo('Exercice 4 <br/>');
-	$var=1;
-	while($var<10){
-		print($var.'<br/>');
-		$var*=1.5;
+	echo('<br/> Exercice 4 <br/>');
+	$var6=1;
+	while($var6<10){
+		print($var6.'<br/>');
+		$var6*=1.5;
 	}
 	?>
+
 	<?php
 	echo('Exercice 5 <br/>');
-	for($var=0;$var<=15;$var++){
-		print("**On y arrive presque**<br/>");
+	for($int=1;$int<=15;$int++){
+		print("On y arrive presque<br/>");
 	}
 	?>
 
 	<?php
 	echo('Exercice 6 <br/>');
-	for($var=20;$var!=0;$var--){
-		print("**C'est presque bon**<br/>");		
+	for($int2=20;$int2>=0;$int2--){
+		print("C'est presque bon<br/>");		
 	}
 	?>
 
 	<?php
 	echo('Exercice 7 <br/>');
-	for($var=1;$var<=100;$var+=15){
-		print("**On tient le bon bout**<br/>");	
+	for($int3=1;$int3<=100;$int3+=15){
+		print("*On tient le bon bout*<br/>");	
 	}
 	?>
 
 	<?php
 	echo('Exercice 8 <br/>');
-	for($var=200;$var>=0;$var-=12){
+	for($int4=200;$int4>=0;$int4-=12){
 		print('**Enfin!!!!** <br/>');
 	}
 	?>
